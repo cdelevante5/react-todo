3f21b8222e1bc0aa10718d17472f404408c058a5
Cynthia Delevante <cynthiadelevante@Cynthias-MacBook-Air.local>
2024-03-04 21:01:34 -0500

    made todo list

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const todolist= [{id:1, title:"Complete Assignment"}, {id:2, title:"Clean Bedroom"}, {id:3, title:"Fold Laundry"}]
+
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>to-do list hello</h1>
+      <h1>Todo List</h1> 
+      <ul>
+      {todolist.map(item => {
+        console.log(item)
+        console.log(item.title)
+        return( 
+          <li key = {item.id}> 
+
+          {item.title}
+          </li>
+          
+        )
+      })}
+      
+      </ul>
     </>
   )
 }
